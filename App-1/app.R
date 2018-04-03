f1d39db7dcec0ed9b58ba5cccaae584a3cee9800
mahokkit <ma.edward@hotmail.com>
2018-04-03 17:25:46 -0300

    demo 1

---
+++ App-1/app.R
@@ -0,0 +1,70 @@
+library(shiny)
+
+# Define UI for app that draws a histogram ----
+ui <- fluidPage(
+  
+  # App title
+  titlePanel("Hello Shiny!"),
+  
+  # Sidebar layout with input and output definitions
+  sidebarLayout(
+    
+    # Sidebar panel for inputs
+    sidebarPanel(
+      
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "bins",
+                  #slider title
+                  label = "Number of bins:",
+                  
+                  # min value
+                  min = 1,
+                  
+                  # max value
+                  max = 50,
+                  
+                  # default value
+                  value = 30)
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Histogram ----
+      plotOutput(outputId = "distPlot")
+      
+    )
+  )
+)
+
+# Define server logic required to draw a histogram ----
+server <- function(input, output) {
+  
+  # Histogram of the Old Faithful Geyser Data ----
+  # with requested number of bins
+  # This expression that generates a histogram is wrapped in a call
+  # to renderPlot to indicate that:
+  #
+  # 1. It is "reactive" and therefore should be automatically
+  #    re-executed when inputs (input$bins) change
+  # 2. Its output type is a plot
+  output$distPlot <- renderPlot({
+    
+    x    <- faithful$waiting
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # color code can be in hex
+    hist(x, breaks = bins, col = "red", border = "white",
+         
+         # title on X, Y is frequency
+         xlab = "Waiting time to next eruption (in mins)",
+         
+         # histogram title
+         main = "Histogram of waiting times")
+    
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
