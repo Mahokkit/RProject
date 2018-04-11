0c40393a3136fae53921a0c2f82f16a770ad2334
mahokkit <ma.edward@hotmail.com>
2018-04-11 18:20:39 -0300

    made some combination

--- census-app/app.R
+++ census-app/app.R
@@ -5,15 +5,13 @@ library(mapproj)
 source("helpers.R")
 counties <- readRDS("data/counties.rds")
 # Define UI ----
-ui <- fluidPage(
-  titlePanel(strong("censusVis")),
+ui <- fluidPage( title = "2010 US Census Visualization",
+  titlePanel("2010 US Census Visualization"),
+  
   sidebarLayout(
     sidebarPanel(
-      helpText("Create demographic 
-               maps with information from the 2010 US Census."),
-      
       selectInput("select", 
-                  h3(strong("Choose a variable to display")),
+                  h3(strong("Choose a population to display")),
                   choices = list(
                     "Percent White",
                     "Percent Black",
@@ -23,13 +21,31 @@ ui <- fluidPage(
       ),
       sliderInput("num",
                   h3(strong("Range of Interest")),
-                  min = 0, max = 100, value = c(0, 50))
+                  min = 0, max = 100, value = c(0, 50)),
+      plotOutput("map")
       ),
     
-    mainPanel(plotOutput("map"))
+    mainPanel(
+      p("Shiny is an R package that makes it",em("incredibly easy ")," to build interactive web applications straight from R"),
+      p("It is an interactive approach to tell your data story, and allow end users to interact with your data and analysis."),
+      
+      h2("Features"),
+      p("- Combines the computational power of R"),
+      p("- Built in Responsive Design try it on your mobile device or tablet!"),
+      p("- Host your Shiny app on the web by using your own servers or RStudio's hosting service like this", 
+        strong("one")," with ShinyApps.io"),
+      p("- Shiny applications are automatically 'live' in the same way that ", 
+        strong("spreadsheets"),
+        " are live. Outputs change instantly as users modify inputs, without requiring a reload of the browser.  Try it out!"),
+      img(src = "rstudio.png", height = 80, width = 250, align = "center"),
+      br(),
+      "Shiny is a product of ",span("RStudio",style = "color:blue"),
+      p("To learn more visit the ",
+        a("Shiny homepage.", 
+          href = "http://shiny.rstudio.com"))
+      )
   )
   )
-
 
 # Define server logic ----
 server <- function(input, output) {
@@ -37,14 +53,14 @@ server <- function(input, output) {
     data <- switch(input$select,
                    "Percent White" = counties$white,
                    "Percent Black" = counties$black,
-                   "Percent Hispanic" = counties$hispanic,,
+                   "Percent Hispanic" = counties$hispanic,
                    "Percent Asian" = counties$asian)
     
     color <- switch(input$select,
                    "Percent White" = "darkgreen",
                    "Percent Black" = "black",
-                   "Percent Hispanic" = "brown",
-                   "Percent Asian" = "darkorange")
+                   "Percent Hispanic" = "red",
+                   "Percent Asian" = "brown")
     
     legend <- switch(input$select,
                     "Percent White" = "% White",
--- shiny_template.R
+++ shiny_template.R
@@ -7,7 +7,7 @@ ui <- fluidPage(
 
 # Define server logic ----
 server <- function(input, output) {
-  
+  output$scatter.smooth(x=cars$speed, y=cars$dist, main="Dist ~ Speed")
 }
 
 # Run the app ----
